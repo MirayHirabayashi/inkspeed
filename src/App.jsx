463b419ecc8513eaa6690f2ef788cfeb9ea9ae7c
Miray Hirabayashi <miraykgungor@gmail.com>
2025-06-16 12:45:44 -0500

    chore: add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TypingSpeedTest from "./components/TypingSpeedTest";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return ( 
@@ -7,6 +8,7 @@ function App() {
   <div className="flex flex-col items-center bg-black w-screen h-screen">
     <h1 className="text-[50px] text-white font-sans mt-5">inkspeed</h1>
     <TypingSpeedTest />
+    <Analytics />
   </div>
   </>
   )
